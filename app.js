c8e0fdb13c5cadca47cf79fc6ba1f70d414ca9ab
Eghinner <eghinner@gmail.com>
2021-10-01 18:27:43 -0700

    validation repair

--- app.js
+++ app.js
@@ -1,59 +1,78 @@
-var form = document.querySelector("#form");
-var input_name = document.querySelector("#name");
-var input_last = document.querySelector("#last");
-var input_email = document.querySelector("#email");
-var input_passw = document.querySelector("#passw");
+const 	form 	= document.querySelector("#form"),
+input_name 	= document.querySelector("#name"),
+input_last 	= document.querySelector("#last"),
+input_email = document.querySelector("#email"),
+input_passw = document.querySelector("#passw");
 
-form.addEventListener("submit", function () {
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
 
-	var name = document.querySelector("#name").value;
-	var last = document.querySelector("#last").value;
-	var email = document.querySelector("#email").value;
-	var passw = document.querySelector("#passw").value;
-
-
-
-	if (name.length==0) {
-		input_name.style.borderColor = "red";
-		input_name.style.backgroundImage = "url('images/icon-error.svg')";
-		input_name.style.backgroundPosition = "350px";
-		input_name.style.backgroundRepeat = "no-repeat";
-	} else {
-		input_name.style.borderColor = "var(--green)";
-		name;
-	}
-
-	if (last.length==0) {
-		
-		input_last.style.borderColor = "red";
-		input_last.style.backgroundImage = "url('images/icon-error.svg')";
-		input_last.style.backgroundPosition = "350px";
-		input_last.style.backgroundRepeat = "no-repeat";
-
-	} else {
-		input_last.style.borderColor = "var(--green)";
-	}
-
-	if (email.length==0 ||
-	 !(email.includes("@gmail.com", -10) || email.includes("@hotmail.com") || email.includes("@outlook.com"))) {
-		input_email.style.borderColor = "red";
-		input_email.style.backgroundImage = "url('images/icon-error.svg')";
-		input_email.style.backgroundPosition = "350px";
-		input_email.style.backgroundRepeat = "no-repeat";
-
-	} else {
-		input_email.style.borderColor = "var(--green)";
-	}
-
-	if (passw.length==0) {
-		
-		input_passw.style.borderColor = "red";
-		input_passw.style.backgroundImage = "url('images/icon-error.svg')";
-		input_passw.style.backgroundPosition = "350px";
-		input_passw.style.backgroundRepeat = "no-repeat";
-
-	} else {
-		input_passw.style.borderColor = "var(--green)";
-	}
-	
+	checkInputs();
 });
+
+function checkInputs() {
+	const name = input_name.value.trim();
+	const last = input_last.value.trim();
+	const email = input_email.value.trim();
+	const passw = input_passw.value.trim();
+
+	// Valid Name
+	if (name.length == 0) {
+		setErrorFor(input_name);
+	} else {
+		setSuccessFor(input_name);
+	}
+	// Valid Last Name
+	if (last.length==0) {
+		setErrorFor(input_last);
+	} else {
+		setSuccessFor(input_last);
+	}
+	// Valid email with regex
+	if (email.length==0) {
+		setErrorFor(input_email);
+	}	else if (!IsEmail(email)){
+		setErrorFor(input_email);
+	}	else {
+		setSuccessFor(input_email);
+	}
+	// Valid password with regex (password>=8 && [A-Za-z] && [0-9] && [@$!%#-?&])
+	if (passw.length==0) {
+		setErrorFor(input_passw);
+	}	else if (!IsPassw(passw)){
+		setErrorFor(input_passw);
+	}	else {
+		setSuccessFor(input_passw);
+	}
+};
+
+
+
+
+function IsEmail(email) {
+	return /^[a-z0-9]+\.?\w*@[a-z]+(\.\w{2,3})?\.\w{2,4}$/.test(email);
+}
+
+function IsPassw(password) {
+	return /^(?=.*\d)(?=.*[!@#$%^&*\-])(?=.*\w).{8,}$/.test(password);
+}
+
+// -----------------------------
+
+function setErrorFor(input) {
+	// const formControl = input.parentElement;
+	// const small = formControl.querySelector('small');
+	// formControl.className = 'form-control error';
+	// small.innerText = message;
+
+	input.style.borderColor = "red";
+	input.style.backgroundImage = "url('images/icon-error.svg')";
+	input.style.backgroundPosition = "95% 50%";
+	input.style.backgroundRepeat = "no-repeat";
+}
+
+function setSuccessFor(input) {
+	// const formControl = input.parentElement;
+	// formControl.className = 'form-control success';
+	input.style.borderColor = "var(--green)";
+}
